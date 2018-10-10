bcbc4e7fb4d125ff66fd283e816bc7fea52dcefb
xianshenglu <xianshenglu@foxmail.com>
2018-10-10 22:14:42 +0800

    modify api

--- demo/kugou/src/assets/js/api.js
+++ demo/kugou/src/assets/js/api.js
@@ -1,16 +1,17 @@
 //doc: https://github.com/xCss/JsonBird
 let proxyUrl = 'https://bird.ioliu.cn/v1?url=';
+let baseApiHost = 'http://m.kugou.com'
 let api = {
-  newSong: 'http://m.kugou.com/?json=true',
-  rankList: 'http://m.kugou.com/rank/list&json=true',
-  rankInfo: 'http://m.kugou.com/rank/info/?page=1&json=true&rankid=',
-  songList: 'http://m.kugou.com/plist/index&json=true',
-  songListInfo: 'http://m.kugou.com/plist/list/songListId?json=true',
-  singerCategory: 'http://m.kugou.com/singer/class&json=true',
+  newSong: baseApiHost + '/?json=true',
+  rankList: baseApiHost + '/rank/list&json=true',
+  rankInfo: baseApiHost + '/rank/info/?page=1&json=true&rankid=',
+  songList: baseApiHost + '/plist/index&json=true',
+  songListInfo: baseApiHost + '/plist/list/songListId?json=true',
+  singerCategory: baseApiHost + '/singer/class&json=true',
   singerCategoryInfo:
-    'http://m.kugou.com/singer/list/singerCategoryInfoId?json=true',
-  singerInfo: 'http://m.kugou.com/singer/info/singerId&json=true',
-  song_info: 'http://m.kugou.com/app/i/getSongInfo.php?cmd=playInfo&hash=',
+    baseApiHost + '/singer/list/singerCategoryInfoId?json=true',
+  singerInfo: baseApiHost + '/singer/info/singerId&json=true',
+  song_info: baseApiHost + '/app/i/getSongInfo.php?cmd=playInfo&hash=',
   song_info_lyric: 'http://www.kugou.com/yy/index.php?r=play/getdata&hash=',
   hotSearch:
     'http://mobilecdn.kugou.com/api/v3/search/hot?format=json&plat=0&count=30',
