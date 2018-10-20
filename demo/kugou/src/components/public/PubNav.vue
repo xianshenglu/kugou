35c870e21cb4b1db641a2d0918865518fc73a249
xianshenglu <xianshenglu@foxmail.com>
2018-10-20 20:17:43 +0800

    simplized PubNav html structure and use vue-router active class instead of detecting

--- demo/kugou/src/components/public/PubNav.vue
+++ demo/kugou/src/components/public/PubNav.vue
@@ -1,40 +1,32 @@
 <template>
   <nav class="nav">
-    <ul class="nav__list">
-      <li :class="item.active?'active nav__item':'nav__item'" v-for="(item,index) in navs" :key="index">
-        <router-link class="nav__link" :to="item.link">{{item.text}}</router-link>
-      </li>
-    </ul>
+    <router-link class="nav__link" v-for="(item,index) in navs" :key="index" :to="item.link" exact-active-class="nav__link--active">{{item.text}}</router-link>
   </nav>
 </template>
 
 <script>
 export default {
-  name:'PubNav',
-  props:['navs']
-
+  name: 'PubNav',
+  props: ['navs']
 }
 </script>
 
 <style lang="less" scoped>
-@themeColor: #2ca2f9;
+@import (reference) '../../assets/css/constant.less';
 
 .nav {
   position: relative;
   z-index: 1;
-
-  box-shadow: 0 3px 3px 0 #eee;
-}
-.nav__list {
   display: flex;
 
   box-sizing: border-box;
   width: 100%;
   padding: 0 13px;
+  box-shadow: 0 3px 3px 0 #eee;
 }
-.nav__item {
+.nav__link {
   flex: 1 0 auto;
-
+  display: block;
   box-sizing: border-box;
   height: 58px;
 
@@ -45,17 +37,7 @@ export default {
   font-size: 21px;
   line-height: 56px;
 }
-.nav__item:hover {
-  border-bottom-color: @themeColor;
+.nav__link--active {
+  border-bottom-color: @theme-color;
 }
-.nav__link {
-  display: block;
-
-  width: 100%;
-  height: 100%;
-}
-
-
-
-
 </style>
