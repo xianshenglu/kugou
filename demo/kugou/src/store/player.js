a00ebb72e978ba1aa2cfc113e594085046c96154
xianshenglu <xianshenglu@foxmail.com>
2018-10-30 21:52:28 +0800

    same with last commit

--- demo/kugou/src/store/player.js
+++ demo/kugou/src/store/player.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 import api from '../assets/js/api'
 import store from './index'
+function getCurMusicIndex(state) {
+  return state.musicList.findIndex(music => music === state.music)
+}
 const player = {
   namespaced: true,
   state: {
@@ -11,9 +14,7 @@ const player = {
     isPlaying: false
   },
   getters: {
-    curMusicIndex: state => {
-      return state.musicList.findIndex(music => music === state.music)
-    }
+    curMusicIndex: getCurMusicIndex
   },
   mutations: {
     findAudioEl(state, el) {
@@ -36,12 +37,12 @@ const player = {
       }
     },
     next(state) {
-      let index = player.getters.curMusicIndex
+      let index = getCurMusicIndex(state)
       index = index === state.musicList.length - 1 ? -1 : index
       store.commit('player/wantPlay', { music: state.musicList[index + 1] })
     },
     prev(state) {
-      let index = player.getters.curMusicIndex
+      let index = getCurMusicIndex(state)
       index = index === 0 ? state.musicList.length : index
       store.commit('player/wantPlay', { music: state.musicList[index - 1] })
     }
