55a2e77a935dde1dee07bf4cc62cc64a054d3630
xianshenglu <xianshenglu@foxmail.com>
2018-11-04 18:19:46 +0800

    add BackgroundImage blur for PlayMax

--- demo/kugou/src/components/player/PlayerMax.vue
+++ demo/kugou/src/components/player/PlayerMax.vue
@@ -1,41 +1,42 @@
 <template>
-  <section class="player" :style="playerBgImg">
-    <h6 class="player__song_name">{{songName}}</h6>
-    <img :src="singerImg" class="player__singer_img">
-    <PlayerLyrics class="player__lyrics"/>
-    <PlayerProgress class="player__progress"/>
-    <div class="player__buttons">
-      <PrevButton class="player__btn_prev"/>
-      <PlayButton class="player__btn_status"/>
-      <NextButton class="play__btn_next"/>
+  <section class="player">
+    <div class="player__mask" :style="playerBgImg"></div>
+    <div class="player__cont">
+      <h6 class="player__song_name">{{songName}}</h6>
+      <img :src="singerImg" class="player__singer_img">
+      <PlayerLyrics class="player__lyrics"/>
+      <PlayerProgress class="player__progress"/>
+      <div class="player__buttons">
+        <PrevButton class="player__btn_prev"/>
+        <PlayButton class="player__btn_status"/>
+        <NextButton class="play__btn_next"/>
+      </div>
+      <button class="player__download"></button>
     </div>
-    <button class="player__download"></button>
   </section>
 </template>
 
 <script>
+import PlayerLyrics from './PlayerLyrics'
+import PlayerProgress from './PlayerProgress'
 import PlayButton from './PlayButton'
 import NextButton from './NextButton'
 import PrevButton from './PrevButton'
-import PlayerLyrics from './PlayerLyrics'
-import PlayerProgress from './PlayerProgress'
 import { mapState, mapGetters, mapMutations } from 'vuex'
 export default {
   name: 'PlayerMax',
   components: {
+    PlayerLyrics,
+    PlayerProgress,
     PlayButton,
     NextButton,
-    PrevButton,
-    PlayerLyrics,
-    PlayerProgress
+    PrevButton
   },
   computed: {
     ...mapState('player', ['audioEl']),
     ...mapGetters('player', ['songName', 'singerName', 'singerImg']),
     playerBgImg() {
-      return `background-image:linear-gradient(to right,rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url(${
-        this.singerImg
-      });`
+      return `background-image:url(${this.singerImg});`
     }
   },
   mounted() {
@@ -59,12 +60,27 @@ export default {
 <style scoped lang="less">
 @import (reference) '../../assets/css/constant.less';
 .player {
+  position: relative;
+}
+.player__mask {
+  position: absolute;
+  left: 0;
+  top: 0;
+  filter: blur(15px);
+  width: 100%;
+  height: 100%;
+  display: block;
   background-repeat: no-repeat;
   background-size: auto 100%;
-  background-position: 50% 100%;
+  background-position: center;
+  z-index: -1;
+}
+.player__cont {
   display: flex;
   flex-flow: column;
   align-items: center;
+  background: rgba(0, 0, 0, 0.6);
+  height: 100%;
 }
 .player__song_name {
   height: 54px;
