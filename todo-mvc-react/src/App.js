083e223ac9f8102c9a4da0d3cc340038ea48ae96
xianshenglu <xianshenglu@foxmail.com>
2019-01-10 18:10:10 +0800

    import components and store in App.js and watch the state change

--- todo-mvc-react/src/App.js
+++ todo-mvc-react/src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import './assets/reset.css'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import TodoInput from './components/TodoInput'
+import TodoFilter from './components/TodoFilter'
+import TodoListContainer from './containers/TodoListContainer'
+import store from './reducers/index'
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = store.getState()
+    this.unsubscribe = store.subscribe(() => {
+      this.setState(() => store.getState())
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <TodoInput />
+        <TodoListContainer
+          todos={this.state.todos}
+          filterMode={this.state.filterMode}
+        />
+        <TodoFilter mode={this.state.filterMode} />
+        <Footer />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
