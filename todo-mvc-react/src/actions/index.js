6a84c3e36eb861b0244f080cbc79bd4b88367d3c
xianshenglu <xianshenglu@foxmail.com>
2019-01-10 13:33:45 +0800

    add actions

---
+++ todo-mvc-react/src/actions/index.js
@@ -0,0 +1,5 @@
+import * as type from '../constants/ActionTypes'
+export const addTodo = text => ({ type: type.ADD_TODO, text })
+export const removeTodo = id => ({ type: type.REMOVE_TODO, id })
+export const alterTodo = (id, text) => ({ type: type.ADD_TODO, id, text })
+export const setFilterMode = mode => ({ type: type.SET_FILTER_MODE, mode })
