0b1a3eb44401645ba0e8e0ceaf36d4123ec2309c
xianshenglu <xianshenglu@foxmail.com>
2019-01-10 21:24:55 +0800

    simplify code

--- todo-mvc-react/src/components/TodoFilter.js
+++ todo-mvc-react/src/components/TodoFilter.js
@@ -2,37 +2,35 @@ import React from 'react'
 import { SHOW_ALL, SHOW_ACTIVE, SHOW_COMPLETED } from '../constants/FilterMode'
 import './TodoFilter.css'
 const classNames = require('classnames')
-class TodoFilter extends React.Component {
-  render() {
-    let mode = this.props.mode
-    return (
-      <div className="TodoFilter">
-        <span
-          className={classNames('TodoFilter__mode', {
-            'TodoFilter__mode--selected': mode === SHOW_ALL
-          })}
-          onClick={() => this.props.setFilterMode(SHOW_ALL)}
-        >
-          all
-        </span>
-        <span
-          className={classNames('TodoFilter__mode', {
-            'TodoFilter__mode--selected': mode === SHOW_ACTIVE
-          })}
-          onClick={() => this.props.setFilterMode(SHOW_ACTIVE)}
-        >
-          active
-        </span>
-        <span
-          className={classNames('TodoFilter__mode', {
-            'TodoFilter__mode--selected': mode === SHOW_COMPLETED
-          })}
-          onClick={() => this.props.setFilterMode(SHOW_COMPLETED)}
-        >
-          completed
-        </span>
-      </div>
-    )
-  }
+function TodoFilter(props) {
+  return (
+    <div className="TodoFilter">
+      <span
+        className={classNames('TodoFilter__mode', {
+          'TodoFilter__mode--selected': props.mode === SHOW_ALL
+        })}
+        onClick={() => props.setFilterMode(SHOW_ALL)}
+      >
+        all
+      </span>
+      <span
+        className={classNames('TodoFilter__mode', {
+          'TodoFilter__mode--selected': props.mode === SHOW_ACTIVE
+        })}
+        onClick={() => props.setFilterMode(SHOW_ACTIVE)}
+      >
+        active
+      </span>
+      <span
+        className={classNames('TodoFilter__mode', {
+          'TodoFilter__mode--selected': props.mode === SHOW_COMPLETED
+        })}
+        onClick={() => props.setFilterMode(SHOW_COMPLETED)}
+      >
+        completed
+      </span>
+    </div>
+  )
 }
+
 export default TodoFilter
