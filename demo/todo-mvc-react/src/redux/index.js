9e5e204b751c1be69393ae9d29a46433fc5dc2e3
xianshenglu <xianshenglu@foxmail.com>
2019-01-11 17:54:06 +0800

    extract reducers to redux/reducers/

--- demo/todo-mvc-react/src/redux/index.js
+++ demo/todo-mvc-react/src/redux/index.js
@@ -1,7 +1,6 @@
 import { SHOW_ALL } from '../constants/FilterMode'
-import * as actionType from '../constants/ActionTypes'
-
 import { createStore } from 'redux'
+import { todosHandler, filterModeHandler } from './reducers'
 const initialState = {
   todos: [
     {
@@ -17,35 +16,7 @@ const initialState = {
   ],
   filterMode: SHOW_ALL
 }
-function todosHandler(state, { type, id, text, completed }) {
-  const map = {
-    [actionType.ADD_TODO]() {
-      return state.concat({ id: Date.now(), text, completed: false })
-    },
-    [actionType.REMOVE_TODO]() {
-      let targetTodoIndex = state.findIndex(todo => todo.id === id)
-      if (targetTodoIndex < -1) {
-        return state
-      }
-      let targetTodos = state.slice()
-      targetTodos.splice(targetTodoIndex, 1)
-      return targetTodos
-    },
-    [actionType.ALTER_TODO]() {
-      let targetTodoIndex = state.findIndex(todo => todo.id === id)
-      if (targetTodoIndex < -1) {
-        return state
-      }
-      let targetTodos = state.slice()
-      targetTodos[targetTodoIndex].text = text
-      targetTodos[targetTodoIndex].completed = completed
-    }
-  }
-  return typeof map[type] === 'function' ? map[type]() : state
-}
-function filterModeHandler(state, action) {
-  return action.type === actionType.SET_FILTER_MODE ? action.mode : state
-}
+
 function todoApp(state, action) {
   return {
     todos: todosHandler(state.todos, action),
---
+++ demo/todo-mvc-react/src/redux/reducers/index.js
@@ -0,0 +1,31 @@
+import * as actionType from '../../constants/ActionTypes'
+
+export function todosHandler(state, { type, id, text, completed }) {
+  const map = {
+    [actionType.ADD_TODO]() {
+      return state.concat({ id: Date.now(), text, completed: false })
+    },
+    [actionType.REMOVE_TODO]() {
+      let targetTodoIndex = state.findIndex(todo => todo.id === id)
+      if (targetTodoIndex < -1) {
+        return state
+      }
+      let targetTodos = state.slice()
+      targetTodos.splice(targetTodoIndex, 1)
+      return targetTodos
+    },
+    [actionType.ALTER_TODO]() {
+      let targetTodoIndex = state.findIndex(todo => todo.id === id)
+      if (targetTodoIndex < -1) {
+        return state
+      }
+      let targetTodos = state.slice()
+      targetTodos[targetTodoIndex].text = text
+      targetTodos[targetTodoIndex].completed = completed
+    }
+  }
+  return typeof map[type] === 'function' ? map[type]() : state
+}
+export function filterModeHandler(state, action) {
+  return action.type === actionType.SET_FILTER_MODE ? action.mode : state
+}
