2ef526265378abc147b387dfddb4fb4836165f45
xianshenglu <xianshenglu@foxmail.com>
2019-01-11 17:43:22 +0800

    import reselect and use selectors to transform data with high performance

--- demo/todo-mvc-react/src/containers/TodoListContainer.js
+++ demo/todo-mvc-react/src/containers/TodoListContainer.js
@@ -1,17 +1,10 @@
 import { connect } from 'react-redux'
-import { SHOW_ALL, SHOW_ACTIVE, SHOW_COMPLETED } from '../constants/FilterMode'
 import TodoList from '../components/TodoList'
 import { removeTodo } from '../redux/actions'
-function filterTodos(todos, filterMode) {
-  const map = {
-    [SHOW_ALL]: todos,
-    [SHOW_ACTIVE]: todos.filter(todo => todo.completed === false),
-    [SHOW_COMPLETED]: todos.filter(todo => todo.completed === true)
-  }
-  return map[filterMode]
-}
+import { filterTodos } from '../redux/selectors'
+
 const mapStateToProps = (state, ownProps) => ({
-  todos: filterTodos(state.todos, state.filterMode),
+  todos: filterTodos(state),
   ...ownProps
 })
 const mapDispatchToProps = dispatch => ({
---
+++ demo/todo-mvc-react/src/redux/selectors/index.js
@@ -0,0 +1,19 @@
+import {
+  SHOW_ALL,
+  SHOW_ACTIVE,
+  SHOW_COMPLETED
+} from '../../constants/FilterMode'
+import { createSelector } from 'reselect'
+const todos = state => state.todos
+const filterMode = state => state.filterMode
+export const filterTodos = createSelector(
+  [todos, filterMode],
+  (todos, filterMode) => {
+    const map = {
+      [SHOW_ALL]: todos,
+      [SHOW_ACTIVE]: todos.filter(todo => todo.completed === false),
+      [SHOW_COMPLETED]: todos.filter(todo => todo.completed === true)
+    }
+    return map[filterMode]
+  }
+)
