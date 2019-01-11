cad413e00e4b775ea618cd3461bcec2bc061902d
xianshenglu <xianshenglu@foxmail.com>
2019-01-11 19:48:06 +0800

    use simplest mapDispatchToProps

--- demo/todo-mvc-react/src/containers/TodoListContainer.js
+++ demo/todo-mvc-react/src/containers/TodoListContainer.js
@@ -6,9 +6,19 @@ import { filterTodos } from '../redux/selectors'
 const mapStateToProps = state => ({
   todos: filterTodos(state)
 })
-const mapDispatchToProps = dispatch => ({
-  removeTodo: id => dispatch(removeTodo(id))
-})
+const mapDispatchToProps = { removeTodo }
+// equal code below, would be more convenient when meeting lots of actionCreators
+// const mapDispatchToProps = dispatch =>
+//   bindActionCreators({ removeTodo }, dispatch)
+// or
+// const mapDispatchToProps = dispatch => ({
+//   removeTodo: bindActionCreators(removeTodo,dispatch)
+// })
+// or
+// const mapDispatchToProps = dispatch => ({
+//   removeTodo: id => dispatch(removeTodo(id))
+// })
+
 export default connect(
   mapStateToProps,
   mapDispatchToProps
