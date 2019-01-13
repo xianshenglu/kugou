9d42657ce12cabd029de0e5336749d59233872e0
xianshenglu <xianshenglu@foxmail.com>
2019-01-13 18:43:28 +0800

    add api.js and utils.js

---
+++ demo/kugou-react/src/assets/js/utils.js
@@ -0,0 +1,13 @@
+export function $_xsl__replaceImgUrlSize(str = '', size = this.imgSize) {
+  return str.replace(/\{\s*size\s*\}/, size)
+}
+export function secondToMin(seconds) {
+  if (isNaN(Number(seconds))) {
+    return '00:00'
+  }
+  //todo support more than 1 hour
+  seconds = Math.floor(seconds)
+  let second = seconds % 60
+  let min = Math.floor(seconds / 60)
+  return String(min).padStart(2, '0') + ':' + String(second).padStart(2, '0')
+}
