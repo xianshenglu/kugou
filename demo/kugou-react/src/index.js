0eb9270fb2f34f8a46bfea0c8ec00d274059ca6f
xianshenglu <xianshenglu@foxmail.com>
2019-01-14 17:16:47 +0800

    add router, store in entry index.js

--- demo/kugou-react/src/index.js
+++ demo/kugou-react/src/index.js
@@ -6,8 +6,21 @@ import './assets/css/iconfont.css'
 import './assets/css/base.less'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import { BrowserRouter } from 'react-router-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
+import rootReducer from './redux/reducers'
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter basename="/">
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
