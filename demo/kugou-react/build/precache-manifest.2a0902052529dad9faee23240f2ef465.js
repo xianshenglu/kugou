90574542eb56536c39a018004befecfd24a30791
xianshenglu <xianshenglu@foxmail.com>
2019-01-21 18:50:14 +0800

    update server files

---
+++ demo/kugou-react/build/precache-manifest.2a0902052529dad9faee23240f2ef465.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/vue/demo/kugou-react/build/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/vue/demo/kugou-react/build/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/vue/demo/kugou-react/build/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/vue/demo/kugou-react/build/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f372034add405aa100e7",
+    "url": "/vue/demo/kugou-react/build/static/js/runtime~main.f372034a.js"
+  },
+  {
+    "revision": "27cd7741b0f1e7a21e27",
+    "url": "/vue/demo/kugou-react/build/static/js/main.27cd7741.chunk.js"
+  },
+  {
+    "revision": "e7f7b31ab76188a4ccdc",
+    "url": "/vue/demo/kugou-react/build/static/js/1.e7f7b31a.chunk.js"
+  },
+  {
+    "revision": "27cd7741b0f1e7a21e27",
+    "url": "/vue/demo/kugou-react/build/static/css/main.a70f71e3.chunk.css"
+  },
+  {
+    "revision": "e7f7b31ab76188a4ccdc",
+    "url": "/vue/demo/kugou-react/build/static/css/1.ab000770.chunk.css"
+  },
+  {
+    "revision": "d066f2cff8d0344a2eecf112be67900e",
+    "url": "/vue/demo/kugou-react/build/index.html"
+  }
+];
--- demo/kugou-react/build/service-worker.js
+++ demo/kugou-react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.859abcb7b3f05b80f4203f244d3fa06b.js"
+  "/vue/demo/kugou-react/build/precache-manifest.2a0902052529dad9faee23240f2ef465.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/vue/demo/kugou-react/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
