41e3a7a5ab5c8ed1b19d5be5ffaef8def6dd19a1
luxiansheng <luxiansheng@zto.cn>
2019-04-06 21:02:38 +0800

    update build files

--- demo/kugou/vue.config.js
+++ demo/kugou/vue.config.js
@@ -2,16 +2,6 @@ module.exports = {
   assetsDir: './static/',
   baseUrl: './'
   // devServer: {
-  //   proxy: {
-  //     // proxy all requests whose path starting with /proxy_api to http://m.kugou.com/proxy_api then remove '/proxy_api' string
-  //     '/proxy_api': {
-  //       target: 'http://m.kugou.com/',
-  //       pathRewrite: {
-  //         '^/proxy_api': ''
-  //       }
-  //     }
-  //   }
-  //   //or just change the origin to http://m.kugou.com
-  //   // proxy: 'http://m.kugou.com'
+  // todo add https for localhost
   // }
-};
+}
