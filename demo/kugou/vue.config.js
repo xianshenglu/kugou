545e9ed70e4272bc6176d566ee2a9743d3635b91
luxiansheng <luxiansheng@zto.cn>
2019-04-21 20:23:59 +0800

    replace libs with scripts

--- demo/kugou/vue.config.js
+++ demo/kugou/vue.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   assetsDir: './static/',
-  baseUrl: 'https://cdn.jsdelivr.net/gh/xianshenglu/vue@master/demo/kugou/dist/'
+  baseUrl:
+    'https://cdn.jsdelivr.net/gh/xianshenglu/vue@master/demo/kugou/dist/',
   // devServer: {
   // todo add https for localhost
   // }
+  configureWebpack: {
+    externals: {
+      vue: 'Vue',
+      'vue-router': 'VueRouter',
+      vuex: 'Vuex',
+      axios: 'axios'
+    }
+  }
 }
