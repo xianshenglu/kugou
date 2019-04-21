3548f410d25e54b03d4f797f3daeb5f8c0cd37a2
luxiansheng <luxiansheng@zto.cn>
2019-04-21 16:22:41 +0800

    add preconnect for proxy api and dns-prefetch for images

--- demo/kugou/vue.config.js
+++ demo/kugou/vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   assetsDir: './static/',
-  // remember to update cdn path in index.html
   baseUrl: 'https://cdn.jsdelivr.net/gh/xianshenglu/vue@master/demo/kugou/dist/'
   // devServer: {
   // todo add https for localhost
