fe647b69b8c02bc79e5f5a2e054ba96c407000d2
xianshenglu <xianshenglu@foxmail.com>
2019-04-25 22:25:40 +0800

    modify baseUrl config

--- demo/kugou/vue.config.js
+++ demo/kugou/vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   assetsDir: './static/',
-  baseUrl: 'https://cdn.jsdelivr.net/gh/xianshenglu/vue@master/demo/kugou/dist/'
+  baseUrl:
+    process.env.NODE_ENV === 'production'
+      ? 'https://cdn.jsdelivr.net/gh/xianshenglu/vue@master/demo/kugou/dist/'
+      : './'
   // devServer: {
   // todo add https for localhost
   // }
