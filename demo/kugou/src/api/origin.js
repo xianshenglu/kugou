4bfd45f143f725ceda05a46bc32e20e8beaa3fb3
xianshenglu <xianshenglu@foxmail.com>
2019-06-18 21:41:14 +0800

    remove / in the end of origin

--- demo/kugou/src/api/origin.js
+++ demo/kugou/src/api/origin.js
@@ -1,6 +1,5 @@
 //doc: https://github.com/xCss/JsonBird
 export const PROXY_URL = 'https://bird.ioliu.cn/v1?url='
-export const BASE_ORIGIN = PROXY_URL + 'http://m.kugou.com/'
-export const KU_GOU_MOBILE_CDN_ORIGIN =
-  PROXY_URL + 'http://mobilecdn.kugou.com/'
-export const KU_GOU_ORIGIN = PROXY_URL + 'http://www.kugou.com/'
+export const BASE_ORIGIN = PROXY_URL + 'http://m.kugou.com'
+export const KU_GOU_MOBILE_CDN_ORIGIN = PROXY_URL + 'http://mobilecdn.kugou.com'
+export const KU_GOU_ORIGIN = PROXY_URL + 'http://www.kugou.com'
