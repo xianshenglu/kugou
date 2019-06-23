1b96559dc6d3dc13314b37d82c851b9ecdc2ed5c
xianshenglu <xianshenglu@foxmail.com>
2019-06-23 11:52:50 +0800

    remove useless import of old api and axios

--- demo/kugou/src/store/rank.js
+++ demo/kugou/src/store/rank.js
@@ -1,5 +1,3 @@
-import axios from 'axios'
-import api from '../assets/js/api'
 const rank = {
   namespaced: true,
   state: {
--- demo/kugou/src/store/song.js
+++ demo/kugou/src/store/song.js
@@ -1,5 +1,3 @@
-import axios from 'axios'
-import api from '../assets/js/api'
 const song = {
   namespaced: true,
   state: {
