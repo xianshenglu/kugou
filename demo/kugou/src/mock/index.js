ca54c7e63f11f98ec4b6e4f5c254a76e2b642c2e
xianshenglu <xianshenglu@foxmail.com>
2019-06-23 11:55:29 +0800

    refactor mock by upgrading api

--- demo/kugou/src/mock/index.js
+++ demo/kugou/src/mock/index.js
@@ -1,37 +1,45 @@
 import Mock from 'mockjs'
-import api from '../assets/js/api'
+import { getNewSong } from '../api/newSong'
 import newSong from './newSong'
+import { getRankList } from '../api/rankList'
 import rankList from './rankList'
+import { getSongList } from '../api/songList'
 import songList from './songList'
+import { getSingerCategory } from '../api/singerCategory'
 import singerCategory from './singerCategory'
+import { getSongLyric } from '../api/playerMax'
 import songLyric from './songLyric'
+import { getRankInfo } from '../api/rankInfo'
 import rankInfo from './rankInfo'
+import { getSongListInfo } from '../api/songListInfo'
 import songListInfo from './songListInfo'
+import { getSingerList } from '../api/singerList'
 import singerList from './singerList'
+import { getSingerInfo } from '../api/singerInfo'
 import singerInfo from './singerInfo'
+import { getHotSearch, getSearchResult } from '../api/search'
 import hotSearch from './hotSearch'
 import searchResult from './searchResult'
 
-Mock.mock(api.newSong, newSong)
-Mock.mock(api.rankList, rankList)
-Mock.mock(api.songList, songList)
-Mock.mock(api.singerCategory, singerCategory)
-Mock.mock(api.rankInfo + rankList.rank.list[0].rankid, rankInfo)
-Mock.mock(
-  api.songListInfo.replace('songListId', songList.plist.list.info[0].specialid),
-  songListInfo
-)
-Mock.mock(
-  api.singerList.replace('singerListId', singerCategory.list[0].classid),
-  singerList
-)
-Mock.mock(
-  api.singerInfo.replace('singerId', singerList.singers.list.info[0].singerid),
-  singerInfo
-)
-Mock.mock(api.songLyric + newSong.data[0].hash, songLyric)
-Mock.mock(api.hotSearch, hotSearch)
-Mock.mock(
-  api.searchResult + encodeURIComponent(hotSearch.data.info[0].keyword),
-  searchResult
-)
+// fix mock bug
+Mock.XHR.prototype.__send = Mock.XHR.prototype.send
+Mock.XHR.prototype.send = function() {
+  if (this.custom.xhr) {
+    this.custom.xhr.withCredentials = this.withCredentials || false
+  }
+  this.__send.apply(this, arguments)
+}
+function getUrlReg(url) {
+  return new RegExp(url.replace(/\//g, '\\/').replace(/\?/, '\\?'))
+}
+Mock.mock(getNewSong.url + '&json=true', newSong)
+Mock.mock(getUrlReg(getRankList.url), rankList)
+Mock.mock(getUrlReg(getSongList.url), songList)
+Mock.mock(getUrlReg(getSingerCategory.url), singerCategory)
+Mock.mock(getUrlReg(getRankInfo.url), rankInfo)
+Mock.mock(getUrlReg(getSongListInfo.url), songListInfo)
+Mock.mock(getUrlReg(getSingerList.url), singerList)
+Mock.mock(getUrlReg(getSingerInfo.url), singerInfo)
+Mock.mock(getUrlReg(getSongLyric.url), songLyric)
+Mock.mock(getUrlReg(getHotSearch.url), hotSearch)
+Mock.mock(getUrlReg(getSearchResult.url), searchResult)
