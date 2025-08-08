d0001243c1deb56577c25deadf6a03fbad71db8f
xianshenglu <xianshenglu@qq.com>
2025-08-08 22:59:33 +0800

    feat(react-vite): add global error boundary

--- demo/react-vite/src/App.tsx
+++ demo/react-vite/src/App.tsx
@@ -1,12 +1,12 @@
-import { Component } from "react";
+import { Component } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import classNames from "classnames";
-import "./App.less";
-import AppHeader from "./components/public/AppHeader";
-import PlayerMedContainer from "./containers/player/PlayerMedContainer";
-import NewSongsContainer from "./containers/newSong/NewSongsContainer";
-import RankListContainer from "./containers/rank/RankListContainer";
-import SongListContainer from "./containers/song/SongListContainer";
+import classNames from 'classnames'
+import './App.less'
+import AppHeader from './components/public/AppHeader'
+import PlayerMedContainer from './containers/player/PlayerMedContainer'
+import NewSongsContainer from './containers/newSong/NewSongsContainer'
+import RankListContainer from './containers/rank/RankListContainer'
+import SongListContainer from './containers/song/SongListContainer'
 import {
   // root,
   newSongs,
@@ -18,57 +18,64 @@ import {
   singerList,
   singerInfo,
   search,
-  player,
-} from "./constants/router";
-import SingerCategoriesContainer from "./containers/singer/SingerCategoriesContainer";
-import RankInfoContainer from "./containers/rank/RankInfoContainer";
-import SongListInfoContainer from "./containers/song/SongListInfoContainer";
-import SingerListContainer from "./containers/singer/SingerListContainer";
-import SingerInfoContainer from "./containers/singer/SingerInfoContainer";
-import SearchContainer from "./containers/search/searchContainer";
-import PlayerMaxContainer from "./containers/player/PlayerMaxContainer";
-import AppNavContainer from "./containers/public/AppNavContainer";
+  player
+} from './constants/router'
+import SingerCategoriesContainer from './containers/singer/SingerCategoriesContainer'
+import RankInfoContainer from './containers/rank/RankInfoContainer'
+import SongListInfoContainer from './containers/song/SongListInfoContainer'
+import SingerListContainer from './containers/singer/SingerListContainer'
+import SingerInfoContainer from './containers/singer/SingerInfoContainer'
+import SearchContainer from './containers/search/searchContainer'
+import PlayerMaxContainer from './containers/player/PlayerMaxContainer'
+import AppNavContainer from './containers/public/AppNavContainer'
+import { ErrorBoundary } from 'react-error-boundary'
 
 class App extends Component {
   render() {
-    const { isPlayerMedShow, isAppNavShow } = this.props as any;
-    const mainClassName = classNames("App__main", {
-      "App__main--underNav": isAppNavShow,
-    });
+    const { isPlayerMedShow, isAppNavShow } = this.props as any
+    const mainClassName = classNames('App__main', {
+      'App__main--underNav': isAppNavShow
+    })
     return (
       <div className="App">
         <AppHeader />
         <AppNavContainer />
         {isPlayerMedShow ? <PlayerMedContainer /> : undefined}
         <main className={mainClassName}>
-          <Routes>
-            <Route path={player} element={<PlayerMaxContainer />} />
-            <Route path={newSongs} element={<NewSongsContainer />} />
-            <Route path={rankList} element={<RankListContainer />} />
-            <Route path={songList} element={<SongListContainer />} />
-            <Route
-              path={singerCategories}
-              element={<SingerCategoriesContainer />}
-            />
-            <Route path={rankInfo + ':id'} element={<RankInfoContainer />} />
-            <Route
-              path={songListInfo + ':id'}
-              element={<SongListInfoContainer />}
-            />
-            <Route
-              path={singerList + ':id'}
-              element={<SingerListContainer />}
-            />
-            <Route
-              path={singerInfo + ':singerListId/:id'}
-              element={<SingerInfoContainer />}
-            />
-            <Route path={search} element={<SearchContainer />} />
-          </Routes>
+          <ErrorBoundary
+            fallback={
+              <div className="main_error_boundary">请回首页或联系管理员！</div>
+            }
+          >
+            <Routes>
+              <Route path={player} element={<PlayerMaxContainer />} />
+              <Route path={newSongs} element={<NewSongsContainer />} />
+              <Route path={rankList} element={<RankListContainer />} />
+              <Route path={songList} element={<SongListContainer />} />
+              <Route
+                path={singerCategories}
+                element={<SingerCategoriesContainer />}
+              />
+              <Route path={rankInfo + ':id'} element={<RankInfoContainer />} />
+              <Route
+                path={songListInfo + ':id'}
+                element={<SongListInfoContainer />}
+              />
+              <Route
+                path={singerList + ':id'}
+                element={<SingerListContainer />}
+              />
+              <Route
+                path={singerInfo + ':singerListId/:id'}
+                element={<SingerInfoContainer />}
+              />
+              <Route path={search} element={<SearchContainer />} />
+            </Routes>
+          </ErrorBoundary>
         </main>
       </div>
     )
   }
 }
 
-export default App;
+export default App
