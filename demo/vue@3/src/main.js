99c968908ed026fdcd652c07417a6968b3dd6113
xianshenglu <xianshenglu@qq.com>
2025-08-16 21:36:37 +0800

    chore(vue@3): remove GLOBAL_PROTOTYPE of window

--- demo/vue@3/src/main.js
+++ demo/vue@3/src/main.js
@@ -1,4 +1,4 @@
-import Vue, { h, createApp } from 'vue'
+import { h, createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
@@ -9,10 +9,7 @@ import './styles/index.less'
 // use mock instead of real http request when in development
 
 import '@shared/mock/index'
-
 Vue.config.productionTip = false
-Vue.prototype.window = window
-
 const app = createApp({
   render: () => h(App)
 })
