38e030fd4f3559c33826a34c4fb30233bbc53c53
xianshenglu <xianshenglu@qq.com>
2025-08-17 13:57:26 +0800

    chore(vue@3): remove @vue/compat

--- demo/vue@3/vue.config.js
+++ demo/vue@3/vue.config.js
@@ -10,23 +10,6 @@ module.exports = {
   // devServer: {
   // todo add https for localhost
   // }
-  chainWebpack: config => {
-    config.resolve.alias.set('vue', '@vue/compat')
-
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap(options => {
-        return {
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 2
-            }
-          }
-        }
-      })
-  },
   configureWebpack: {
     resolve: {
       alias: {
