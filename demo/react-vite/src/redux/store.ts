ce1e927481736c16abbd05eebcf5943503c0fb62
xianshenglu <xianshenglu@qq.com>
2025-10-27 11:14:39 +0800

    refactor(react-vite): migrate songList/songListInfo redux to @reduxjs/toolkit

--- demo/react-vite/src/redux/store.ts
+++ demo/react-vite/src/redux/store.ts
@@ -1,7 +1,5 @@
 import { baseApi } from './baseApi'
-import songList from './reducers/songList'
 import singerCategories from './reducers/singerCategories'
-import songListInfo from './reducers/songListInfo'
 import singerList from './reducers/singerList'
 import singerInfo from './reducers/singerInfo'
 import { configureStore } from '@reduxjs/toolkit'
@@ -9,9 +7,7 @@ import { configureStore } from '@reduxjs/toolkit'
 const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
-    songList,
     singerCategories,
-    songListInfo,
     singerList,
     singerInfo
   },
