d3db40e3369a02ea1d8880ccfa26d66def80523b
xianshenglu <xianshenglu@qq.com>
2025-11-16 00:05:10 +0800

    perf(react-vite): remove .App__main layout shift by calculating nav display state earlier

--- demo/react-vite/src/shared/stores/useAppNavStore.ts
+++ demo/react-vite/src/shared/stores/useAppNavStore.ts
@@ -4,16 +4,12 @@ import { devtools } from 'zustand/middleware'
 
 type AppNavState = {
   activeIndex: number
-  isShow: boolean
   setActiveNavIndex: (index: number) => void
-  switchNav: (isShow: boolean) => void
 }
 
 const createState: StateCreator<AppNavState> = (set) => ({
   activeIndex: 0,
-  isShow: false,
-  setActiveNavIndex: (index: number) => set(() => ({ activeIndex: index })),
-  switchNav: (isShow: boolean) => set(() => ({ isShow }))
+  setActiveNavIndex: (index: number) => set(() => ({ activeIndex: index }))
 })
 
 const useAppNavStore = create<AppNavState>()(
