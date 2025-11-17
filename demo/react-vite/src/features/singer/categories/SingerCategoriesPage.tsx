07223f42375ca9eecde9818be10aa4662cc67d6e
xianshenglu <xianshenglu@qq.com>
2025-11-17 22:19:26 +0800

    feat(react-vite): add missing loading/error for pages

--- demo/react-vite/src/features/singer/categories/SingerCategoriesPage.tsx
+++ demo/react-vite/src/features/singer/categories/SingerCategoriesPage.tsx
@@ -1,11 +1,16 @@
 import type { FC } from 'react'
 import SingerCategories from './SingerCategories'
 import { useGetSingerCategoriesQuery } from './useSingerCategories'
+import { Loading } from 'src/shared/components/Loading'
 
 const SingerCategoriesPage: FC = () => {
-  const { data: singerCategories = [] } = useGetSingerCategoriesQuery()
+  const { data, error, isLoading } = useGetSingerCategoriesQuery()
 
-  return <SingerCategories singerCategories={singerCategories} />
+  if (isLoading) return <Loading />
+  if (error) return <div>Error loading singer categories</div>
+  if (!data) return null
+
+  return <SingerCategories singerCategories={data} />
 }
 
 export default SingerCategoriesPage
